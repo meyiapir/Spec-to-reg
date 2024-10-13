419134ea97b1d40298dc4d2e915105036add230c
Martin <mmeyiapir@gmail.com>
2024-10-13 07:23:49 +0300

    init

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,12 @@ FROM python:3.12.2-alpine3.19
 
 WORKDIR /usr/src/app
 
+EXPOSE 8000
+
 COPY requirements.txt .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD python -m Spec-to-reg
+CMD python -m certify
--- certify/API/check_specs.py
+++ certify/API/check_specs.py
@@ -1,16 +1,25 @@
+import json
 import os
 import tempfile
 
-from fastapi import UploadFile, File, Form, APIRouter
+from fastapi import UploadFile, File, Form, APIRouter, Depends
 from fastapi.responses import JSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from certify.ai.langchain_rag_gpt import check_use_cases_against_specifications_gpt
+from certify.database.database import sessionmaker
+from certify.services.mistakes import record_mistake
 
 router = APIRouter(prefix="/process")
 
+async def get_session():
+    async with sessionmaker() as session:
+        yield session
+
 # Маршрут для загрузки файлов и текстовых спецификаций
 @router.post("/check-specifications/")
-async def check_requirements(specifications: str = Form(...), language: str = Form("en"), files: list[UploadFile] = File(...)):
+async def check_requirements(specifications: str = Form(...), language: str = Form("en"), files: list[UploadFile] = File(...),
+                             session: AsyncSession = Depends(get_session)):
     file_paths = []
     try:
         # Сохранение загруженных файлов во временные файлы
@@ -22,12 +31,19 @@ async def check_requirements(specifications: str = Form(...), language: str = Fo
 
         # Проверка требований на соответствие регламентам
         results = check_use_cases_against_specifications_gpt(file_paths, specifications, language)
-
+        async with AsyncSession() as session:
+            if results:
+                issue = str(json.loads(results[0]['response'])['summary']['issues'])
+                await record_mistake(session, issue)
 
         return JSONResponse(content={"results": results})
 
     finally:
+
+
         # Удаление временных файлов
         for path in file_paths:
             if os.path.exists(path):
                 os.remove(path)
+
+
